303a1d3002ed32fd9b2c1dd0efdac9898c2d2848
Maykmora <maykmora12@gmail.com>
2025-07-26 21:51:10 -0600

    Commit 1

---
+++ ejercicio_01.py
@@ -0,0 +1,7 @@
+while True:
+    print("1.Agregar Películas")
+    print("2.Mostrar películas agregadas")
+    print("3.Buscar películas por genero")
+    print("4.Eliminar una película por titulo")
+    print("5.Ver estadísticas del catálogo")
+    print("6.Salir")
