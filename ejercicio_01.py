69c0b96d38ac6eee2422b7882233727004805852
Maykmora <maykmora12@gmail.com>
2025-07-26 21:56:57 -0600

    Commit 2

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -5,3 +5,21 @@ while True:
     print("4.Eliminar una película por titulo")
     print("5.Ver estadísticas del catálogo")
     print("6.Salir")
+    option=input("Seleccione una opción del menú (1-6): ")
+
+    match option:
+        case "1":
+            print()
+        case "2":
+            print()
+        case "3":
+            print()
+        case "4":
+            print()
+        case "5":
+            print()
+        case "6":
+            print("Saliendo del programa...")
+            break
+        case _:
+            print("Error al seleccionar una opción, inténtelo de nuevo")
